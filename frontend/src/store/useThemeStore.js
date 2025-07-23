a5cfa5588679c85b2a9f8d08763c77adca893778
Raj Mishra <120926614+rajgithub21@users.noreply.github.com>
2025-07-23 11:20:34 +0530

    Update useThemeStore.js

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("current-theme") || "night",
+  theme: localStorage.getItem("current-theme") || "forest",
   setTheme: (theme) => {
     localStorage.setItem("current-theme",theme)
     set({ theme });
